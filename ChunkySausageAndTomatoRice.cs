268d76c87b7f2ac6c8be1e88229458fc03066863
callum <cmgjones@outlook.com>
2018-09-05 11:20:40 +0100

    Reduction on calories

--- ChunkySausageAndTomatoRice.cs
+++ ChunkySausageAndTomatoRice.cs
@@ -24,7 +24,7 @@ namespace Eco.Mods.TechTree
         public override string Description                      { get { return "Juicy Sausages On a Tomato Stir Rice"; } } // item description upon hovering over an item
 
         private static Nutrients nutrition = new Nutrients()    { Carbs = 103, Fat = 16, Protein = 24, Vitamins = 20}; // The effects of eating the item for example mashed potatoe would have more carbs than protein where as steak would be the reverse !!Balance is vital!!
-        public override float Calories                          { get { return 655; } } // Number of calories the food will give you >> thew more complicated and resources intesive the recipe the more ballanced and more calories it should give
+        public override float Calories                          { get { return 635; } } // Number of calories the food will give you >> thew more complicated and resources intesive the recipe the more ballanced and more calories it should give
         public override Nutrients Nutrition                     { get { return nutrition; } } //Dont touch this :)
     }
 
