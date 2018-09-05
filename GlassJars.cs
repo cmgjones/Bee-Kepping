31fe93a06fcdda748137a7acca43d02632786e30
callum <cmgjones@outlook.com>
2018-09-05 21:45:15 +0100

    Added Glass Jar recipe

---
+++ GlassJars.cs
@@ -0,0 +1,54 @@
+namespace Eco.Mods.TechTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Eco.Gameplay.Blocks;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.World;
+    using Eco.World.Blocks;
+    using Eco.Gameplay.Pipes;
+
+    [RequiresSkill(typeof(GlassworkingSkill), 1)]
+    public partial class GlassJarsRecipe : Recipe
+    {
+        public GlassJarsRecipe()
+        {
+            this.Products = new CraftingElement[]
+            {
+                new CraftingElement<GlassJarsItem>(),
+            };
+            this.Ingredients = new CraftingElement[]
+            {
+                new CraftingElement<GlassJarsItem>(typeof(GlassJarsEfficiencySkill), 4, GlassJarsEfficiencySkill.MultiplicativeStrategy),
+
+            };
+            this.CraftMinutes = CreateCraftTimeValue(typeof(GlassJarsRecipe), Item.Get<GlassJarsItem>().UILink(), 2, typeof(GlassJarsSpeedSkill));
+            this.Initialize("GlassJars", typeof(GlassJarsRecipe));
+
+            CraftingComponent.AddRecipe(typeof(KilnObject), this);
+        }
+    }
+
+
+    [Serialized]
+    [Weight(1000)]
+    [Currency]
+    public partial class GlassJarsItem :
+    Item
+    {
+        public override string FriendlyName { get { return "GlassJars"; } } 
+        public override string Description { get { return "A jar for storing honey"; } }
+
+    }
+
+}
